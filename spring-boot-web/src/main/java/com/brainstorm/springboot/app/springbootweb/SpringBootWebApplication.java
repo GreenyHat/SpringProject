dcbe84375a1c35badec8cefee7bb615d646c1707
GreenyHat <aramirezgarcia95@gmail.com>
2023-09-19 11:13:00 +0200

    initial-state

---
+++ spring-boot-web/src/main/java/com/brainstorm/springboot/app/springbootweb/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.brainstorm.springboot.app.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
